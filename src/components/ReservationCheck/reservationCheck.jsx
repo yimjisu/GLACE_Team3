1f0b3d343dda7530094570e623a36d86769b6e05
MAC <mac@MACui-MacBookAir.local>
2021-11-21 03:23:20 +0900

    firebase에 유저 정보업데이트 기능 추가했습니다

--- src/components/ReservationCheck/reservationCheck.jsx
+++ src/components/ReservationCheck/reservationCheck.jsx
@@ -4,13 +4,13 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-
-
-const ReservationCheck = ({ 
+import { firestore } from "../../service/firebase";
+const ReservationCheck = ({
     state, setState, showInfo
-    }) => {
-    
-    const headfootStyle = {backgroundColor: "#FFFFFF"};//"#758BFF"};
+}) => {
+
+    const headfootStyle = { backgroundColor: "#758BFF" };
+    const [tasks, setTasks] = useState([]);
 
     const [text1, setText1] = useState('');
     const onChange1 = (e) => {
@@ -30,61 +30,80 @@ const ReservationCheck = ({
     const onClickPrevBtn = () => {
         setState(state - 1);
     }
-    
+
+
+
     function onClickNextBtn() {
         if (text2 == text3) {
+
+
+            firestore.collection("user_info").add({
+                phone: text1,
+                password: text2
+            })
+                .then(() => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+
             setState(state + 1);
         }
+        else {
+            console.log("password correction fail");
+        }
+
     }
 
     return (
         <div>
             <div>
-            <Row>
-                <Col xs={5}>
-                    <Card className={styles.card}>
-                        <Card.Header style={headfootStyle}>
-                            <div className={styles.header}>
-                            {showInfo.name}
-                            </div>
-                        </Card.Header>
-                        <Card.Img className = {styles.cardImg} variant="top" height="500px" src={showInfo.img}/>
-                        <Card.Footer style={headfootStyle}>                    
-                            <div className={styles.footer}>
-                                장소: {showInfo.place}<br/>
-                                날짜: 2021.11.17(수)<br/>
-                                시간: 20:00<br/>
-                                좌석: C1, C2
-                            </div>
-                        </Card.Footer >
-                    </Card>
-                </Col>
+                <Row>
+                    <Col xs={5}>
+                        <Card className={styles.card}>
+                            <Card.Header style={headfootStyle}>
+                                <div className={styles.header}>
+                                    {showInfo.name}
+                                </div>
+                            </Card.Header>
+                            <Card.Img variant="top" height="500px" src={showInfo.img} />
+                            <Card.Footer style={headfootStyle}>
+                                <div className={styles.footer}>
+                                    장소: {showInfo.place}<br />
+                                    날짜: 2021.11.17(수)<br />
+                                    시간: 20:00<br />
+                                    좌석: C1, C2
+                                </div>
+                            </Card.Footer >
+                        </Card>
+                    </Col>
 
-                <Col xs={5}>
-                    <form className={styles.userInfo}>
-                        <div className={styles.title}>비회원 로그인</div><br/>
-                        
-                        <div className={styles.text}>
-                        <b>휴대폰&nbsp;&nbsp;&nbsp;</b>
-                        <input type='text' onChange={onChange1} value={text1}/>
-                        </div>
-                        
-                        <div className={styles.text}>
-                        <b className={styles.text}>비밀번호&nbsp;&nbsp;&nbsp;</b>
-                        <input type='password' onChange={onChange2} value={text2}/>
-                        </div>
+                    <Col xs={5}>
+                        <form className={styles.userInfo}>
+                            <div className={styles.title}>비회원 로그인</div><br />
 
-                        <div className={styles.text}>
-                        <b className={styles.text}>비밀번호 확인&nbsp;&nbsp;&nbsp;</b>
-                        <input className={styles.box} type='password' onChange={onChange3} value={text3}/>
-                        </div>
-                    </form>
-                </Col>
-            </Row>
+                            <div className={styles.text}>
+                                <b>휴대폰&nbsp;&nbsp;&nbsp;</b>
+                                <input type='text' onChange={onChange1} value={text1} />
+                            </div>
+
+                            <div className={styles.text}>
+                                <b className={styles.text}>비밀번호&nbsp;&nbsp;&nbsp;</b>
+                                <input type='password' onChange={onChange2} value={text2} />
+                            </div>
+
+                            <div className={styles.text}>
+                                <b className={styles.text}>비밀번호 확인&nbsp;&nbsp;&nbsp;</b>
+                                <input className={styles.box} type='password' onChange={onChange3} value={text3} />
+                            </div>
+                        </form>
+                    </Col>
+                </Row>
             </div>
 
-            <Button className = {styles.prevBtn} onClick={onClickPrevBtn}>이전</Button>
-            <Button className = {styles.nextBtn} onClick={onClickNextBtn}>완료</Button>
+            <Button className={styles.prevBtn} onClick={onClickPrevBtn}>이전</Button>
+            <Button className={styles.nextBtn} onClick={onClickNextBtn}>완료</Button>
         </div>
     );
 }
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth} from "firebase/auth";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +17,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+//const db = firebase.database();
+const firestore = firebase.firestore();
 
-export default auth;
+export { firestore };
+export default db;
