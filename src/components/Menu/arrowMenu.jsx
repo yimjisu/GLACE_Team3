f8ae5330acc5ede6b72107abc27cc77af66674f1
nubi12336 <79691513+nubi12336@users.noreply.github.com>
2021-11-24 00:32:45 +0900

    delete function which can click arrowmenu

--- src/components/Menu/arrowMenu.jsx
+++ src/components/Menu/arrowMenu.jsx
@@ -5,28 +5,23 @@ const ArrowMenu = ({
     name, state, index, setState
 }) => {
 
-    const onClickMenu = (index) => {
-        console.log(index);
-        setState(index);
-    }
-
     return (<>
     
     {
         state == index ? (
             <div className = {`${styles.progress} ${styles.current}`}
-                onClick = {() => onClickMenu(index)}>
+                >
                 {name}
             </div>
         ) : (
         index < state ? (
             <div className = {`${styles.progress} ${styles.previous}`}
-                onClick = {() => onClickMenu(index)}>
+                >
                 {name}
             </div>
         ) : (
             <div className = {`${styles.progress}`}
-                onClick = {() => onClickMenu(index)}>
+                >
                 {name}
             </div>
         ))
