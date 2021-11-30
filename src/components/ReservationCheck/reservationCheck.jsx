0c697c30e8f577c5d94f2a158323b4afb799d1c4
MAC <mac@MACui-MacBookAir.local>
2021-12-01 03:35:22 +0900

    conflict resolve 했습니다

--- src/components/ReservationCheck/reservationCheck.jsx
+++ src/components/ReservationCheck/reservationCheck.jsx
@@ -6,23 +6,16 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import { SocketContext } from '../../service/socket';
 import { phoneCheck, pwCheck, pwSame } from '../../util/util'
-//import axios from 'axios';
 
 
 
 const ReservationCheck = ({
-    state, setState, showInfo
+    state, setState, showInfo, selectedSeat
 }) => {
-    //const socket = useContext(SocketContext);
-    //const require = createRequire(import.meta.url);
-
-
-    const headfootStyle = { backgroundColor: "#FFFFFF" };//"#758BFF"};
-
+    const socket = useContext(SocketContext);
     const onClickPrevBtn = () => {
         setState(state - 1);
     }
-
 
     function onClickNextBtn() {
         if (!phoneCheck()) {
@@ -38,23 +31,8 @@ const ReservationCheck = ({
             const pw = document.getElementById('pw').value
             const phone = document.getElementById('phone').value
             var data = { phone: phone, password: pw };
-            /*
             console.log("emit")
-            router.get('/:id', function (req, res) {
-                res.send('Received a GET request, param:' + req.params.id);
-            });
-            router.post('/check', function (req, res) {
-                res.send('POST request to the homepage');
-            });*/
-            //socket.emit("user_add", data);
-
-            axios.post('/msg', {
-                user: 'velopert',
-                message: data
-            })
-                .then(response => { console.log(response) })
-                .catch(response => { console.log(response) });
-
+            socket.emit("user_add", data);
             setState(state + 1);
         }
     }
@@ -63,65 +41,64 @@ const ReservationCheck = ({
 
     return (
         <div>
-            <div>
-                <Row>
-                    <Col xs={5}>
-                        <Card className={styles.card}>
-                            <Card.Header style={headfootStyle}>
-                                <div className={styles.header}>
-                                    {showInfo.name}
-                                </div>
-                            </Card.Header>
-                            <Card.Img className={styles.cardImg} variant="top" height="500px" src={showInfo.img} />
-                            <Card.Footer style={headfootStyle}>
-                                <div className={styles.footer}>
-                                    장소: {showInfo.place}<br />
-                                    날짜: 2021.11.17(수)<br />
-                                    시간: 20:00<br />
-                                    좌석: C1, C2
-                                </div>
-                            </Card.Footer >
-                        </Card>
-                    </Col>
+            <div className={styles.flex}>
+                <div className={styles.card}>
+                    <Card>
+                        <Card.Header>
+                            <div className={styles.header}>
+                                {showInfo.name}
+                            </div>
+                        </Card.Header>
+                        <Card.Img variant="top" height="500px" src={showInfo.img} />
+                        <Card.Footer>
+                            <div className={styles.footer}>
+                                장소: {showInfo.place}<br />
+                                날짜: 2021.11.17(수)<br />
+                                시간: 20:00<br />
+                                좌석: {selectedSeat.join(", ")}
+                            </div>
+                        </Card.Footer >
+                    </Card>
+                </div>
 
-                    <Col xs={5}>
-                        <form className={styles.userInfo}>
-                            <div className={styles.title}>비회원 로그인</div><br />
+                <div className={styles.login}>
+                    <form className={styles.userInfo}>
+                        <div className={styles.title}>비회원 로그인</div><br />
 
-                            <table width="750">
-                                <tr>
-                                    <td width="50" align="center">*</td>
-                                    <td width="150" align="left">휴대폰</td>
-                                    <td align="left"><input className={styles.input} id='phone' type="text" size="26" placeholder="번호만 입력(ex.01012345678)"
-                                        onKeyUp={phoneCheck} />
-                                        &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="phoneCheck"></span></td>
-                                </tr>
-                                <tr height="7">
-                                    <td colspan="3"><hr /></td>
-                                </tr>
-                                <tr>
-                                    <td align="center">*</td>
-                                    <td align="left">비밀번호</td>
-                                    <td align="left"><input className={styles.input} id='pw' type="password" size="26" onKeyUp={pwCheck} />
-                                        &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="pwCheck"></span></td>
-                                </tr>
-                                <tr height="7">
-                                    <td colspan="3"><hr /></td>
-                                </tr>
-                                <tr>
-                                    <td align="center">*</td>
-                                    <td align="left">비밀번호 확인</td>
-                                    <td align="left"><input className={styles.input} id='confirmPw' type="password" size="26" onKeyUp={pwSame} />
-                                        &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="pwSame"></span></td>
-                                </tr>
-                            </table>
-                        </form>
-                    </Col>
-                </Row>
+                        <table>
+                            <tr>
+                                <td width="50" align="center">*</td>
+                                <td width="150" align="left">휴대폰</td>
+                                <td align="left"><input className={styles.input} id='phone' type="text" size="26" placeholder="번호만 입력(ex.01012345678)"
+                                    onKeyUp={phoneCheck} />
+                                    &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="phoneCheck"></span></td>
+                            </tr>
+                            <tr height="7">
+                                <td colspan="3"><hr /></td>
+                            </tr>
+                            <tr>
+                                <td align="center">*</td>
+                                <td align="left">비밀번호</td>
+                                <td align="left"><input className={styles.input} id='pw' type="password" size="26" onKeyUp={pwCheck} />
+                                    &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="pwCheck"></span></td>
+                            </tr>
+                            <tr height="7">
+                                <td colspan="3"><hr /></td>
+                            </tr>
+                            <tr>
+                                <td align="center">*</td>
+                                <td align="left">비밀번호 확인</td>
+                                <td align="left"><input className={styles.input} id='confirmPw' type="password" size="26" onKeyUp={pwSame} />
+                                    &nbsp;&nbsp;&nbsp;<span className={styles.explain} id="pwSame"></span></td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
             </div>
-
-            <Button className={styles.prevBtn} onClick={onClickPrevBtn}>이전</Button>
-            <Button className={styles.nextBtn} onClick={onClickNextBtn}>완료</Button>
+            <div>
+                <Button className={styles.prevBtn} onClick={onClickPrevBtn}>이전</Button>
+                <Button className={styles.nextBtn} onClick={onClickNextBtn}>완료</Button>
+            </div>
         </div>
     );
 }
