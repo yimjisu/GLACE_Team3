0f9349944219ffd18678068f21b5507594d61013
MAC <mac@MACui-MacBookAir.local>
2021-12-08 18:12:36 +0900

    제가 작업 한 것중에 쓸데 없는 주석을 제거했습니다

--- app/src/components/UserInfo/userinfo.jsx
+++ app/src/components/UserInfo/userinfo.jsx
@@ -10,7 +10,6 @@ const UserInfo = ({ setState, setUserReservationInfo }) => {
     const pw = document.getElementById("pw").value;
     const phone = document.getElementById("phone").value;
     axios
-      //.post("/user/reservation?phone=" + phone + "&password=" + pw)
       .post("/user/reservation", {
         phone: phone,
         password: pw
