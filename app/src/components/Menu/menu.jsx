e5eccad7a82bc202967be191c951bcb2d11a5925
yimjisu <yimjisu99@gmail.com>
2021-12-08 18:06:26 +0900

    space 예약 완료

--- app/src/components/Menu/menu.jsx
+++ app/src/components/Menu/menu.jsx
@@ -2,7 +2,7 @@ import styles from "./menu.module.css";
 import ArrowMenu from "./arrowMenu";
 
 const Menu = ({ state }) => {
-  const menus = ["공연 선택", "좌석 선택", "개인정보 입력", "예약완료"];
+  const menus = ["공연 선택", "좌석 선택", "개인정보 입력", "예약 완료"];
   return (
     <>
       <div className={styles.background}></div>
